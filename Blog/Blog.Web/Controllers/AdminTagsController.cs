34fcf1c448a3939484f292c9ff799d4f5c55b1af
brocoli05 <mjkimthebest@gmail.com>
2024-05-12 23:49:45 -0400

    first commit

---
+++ Blog/Blog.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,42 @@
+﻿using Blog.Web.Data;
+using Blog.Web.Models.Domain;
+using Blog.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly BlogDbContext blogDbContext;
+
+        public AdminTagsController(BlogDbContext blogDbContext)
+        {
+            this.blogDbContext = blogDbContext;
+        }
+
+        [HttpGet] // localhost:1234/AdminTags/Add
+        public IActionResult Add() // same name with View file (Views > AdminTags > Add)
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest) 
+        {
+            // Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName
+            };
+
+            blogDbContext.Tags.Add(tag);
+            blogDbContext.SaveChanges(); // CRUCIAL!
+
+            //var name = addTagRequest.Name;
+            //var displayName = addTagRequest.DisplayName;
+
+            return View("Add"); // return Add view
+        }
+    }
+}
---
+++ Blog/Blog.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Blog.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Blog.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(); // call the Index.cshtml (Views > Home > Index)
+        }
+
+        public IActionResult Privacy()
+        {
+            return View(); // call the Privacy.cshtml (Views > Home > Privacy)
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
